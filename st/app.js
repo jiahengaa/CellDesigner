89b4f8b9aa1dafb832a2efb908bb315fc67505ac
liujiaheng <2429803999@qq.com>
2019-11-20 21:44:55 +0800

    初步完成表格编辑器构想

--- st/app.js
+++ st/app.js
@@ -1,112 +1,187 @@
-// Nested demo
-var nestedSortables = [].slice.call(
-	document.querySelectorAll(".nested-sortable")
-);
-
 var elements = [];
 elements.push({
-	name: "root",
-	span: 24,
-	elements: [
-		{
-			name: "item1",
-			span: 8,
-			elements: []
-		},
-		{
-			name: "item2",
-			span: 8,
-			elements: []
-		},
-		{
-			name: "item3",
-			span: 8,
-			elements: []
-		}
-	]
+  name: "root",
+  span: 24,
+  type: "col",
+  elements: [
+    {
+      name: "item1",
+      span: 0,
+      type: "row",
+      elements: [
+        {
+          name: "item1",
+          span: 8,
+          type: "col",
+          elements: []
+        },
+        {
+          name: "item2",
+          span: 8,
+          type: "col",
+          elements: []
+        },
+        {
+          name: "item3",
+          span: 8,
+          type: "col",
+          elements: [
+            {
+              name: "item4",
+              span: 8,
+              type: "col",
+              elements: []
+            },
+            {
+              name: "item5",
+              span: 8,
+              type: "col",
+              elements: []
+            },
+            {
+              name: "item6",
+              span: 8,
+              type: "col",
+              elements: []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "itemb1",
+      span: 0,
+      type: "row",
+      elements: [
+        {
+          name: "itemb1",
+          span: 8,
+          type: "col",
+          elements: []
+        },
+        {
+          name: "itemb2",
+          span: 8,
+          type: "col",
+          elements: []
+        },
+        {
+          name: "itemb3",
+          span: 8,
+          type: "col",
+          elements: [
+            {
+              name: "itemb4",
+              span: 8,
+              type: "col",
+              elements: []
+            },
+            {
+              name: "itemb5",
+              span: 8,
+              type: "col",
+              elements: []
+            },
+            {
+              name: "itemb6",
+              span: 8,
+              type: "col",
+              elements: []
+            }
+          ]
+        }
+      ]
+    }
+  ]
 });
 
 function buildContent() {
-	var contentElement = document.createElement("div");
+  var contentElement = document.createElement("div");
 
-	contentElement.classList.add(...["list-group", "col", "nested-sortable"]);
-	makeSortable(contentElement);
+  contentElement.classList.add(...["cellItem", "row"]);
+  makeSortable(contentElement);
 
-	var curRoot = document.getElementById("nestedDemo");
-	elements.forEach(ele => {
-		contentElement = buildChild(contentElement, ele);
-	});
-	curRoot.append(contentElement);
+  var curRoot = document.getElementById("nestedDemo");
+  elements.forEach(ele => {
+    contentElement = buildChild(contentElement, ele);
+  });
+  curRoot.append(contentElement);
 }
 
 function buildChild(parent, ele) {
-	var curEle = document.createElement("div");
-	//新增自定义属性
-	curEle.ele = ele;
-	curEle.classList.add(...["list-group-item", "nested-1", "nested-sortable"]);
-	makeSortable(curEle);
+  var curEle = document.createElement("div");
+  //新增自定义属性
+  curEle.ele = ele;
+  let type = "";
+  if (ele.type === "row") {
+    type = "row";
+  } else {
+    type = "col-md-" + ele.span;
+  }
+  curEle.classList.add(...["cellItem", type]);
+  makeSortable(curEle);
 
-	if (ele.elements.length === 0) {
-		curEle.innerHTML = ele.name;
-		parent.appendChild(curEle);
-	} else {
-		ele.elements.forEach(ele => {
-			curEle = buildChild(curEle, ele);
-			parent.appendChild(curEle);
-		});
-	}
+  if (ele.elements.length === 0) {
+    curEle.innerHTML = ele.name;
+    parent.appendChild(curEle);
+  } else {
+    ele.elements.forEach(ele => {
+      curEle = buildChild(curEle, ele);
+      parent.appendChild(curEle);
+    });
+  }
 
-	return parent;
+  return parent;
 }
 
 buildContent();
 
 function makeSortable(ele) {
-	new Sortable(ele, {
-		group: "nested",
-		animation: 150,
-		fallbackOnBody: true,
-		swapThreshold: 0.65,
-		onAdd: e => {
-			console.log(e);
-		},
-		setData: (dataTransfer, dragEL) => {
-			console.log(dataTransfer);
+  new Sortable(ele, {
+    group: "nested",
+    animation: 150,
+    fallbackOnBody: true,
+    swapThreshold: 0.65,
+    onAdd: e => {
+      console.log(e);
+    },
+    setData: (dataTransfer, dragEL) => {
+      console.log(dataTransfer);
 
-			console.log(dragEL);
-		},
-		onChoose: e => {
-			console.log(e);
-		},
-		onUnchoose: e => {
-			console.log(e);
-		},
-		onStart: e => {
-			console.log(e);
-		},
-		onEnd: e => {
-			console.log(e);
-		},
-		onUpdate: e => {
-			console.log(e);
-		},
-		onSort: e => {
-			console.log(e);
-		},
-		onRemove: e => {
-			console.log(e);
-		},
-		onFilter: e => {
-			console.log(e);
-		},
-		onMove: e => {
-			console.log(e);
-		},
-		onClone: e => {
-			console.log(e);
-		},
-		onChange: e => {
-			console.log(e);
-		}
-	});
+      console.log(dragEL);
+    },
+    onChoose: e => {
+      console.log(e);
+    },
+    onUnchoose: e => {
+      console.log(e);
+    },
+    onStart: e => {
+      console.log(e);
+    },
+    onEnd: e => {
+      console.log(e);
+    },
+    onUpdate: e => {
+      console.log(e);
+    },
+    onSort: e => {
+      console.log(e);
+    },
+    onRemove: e => {
+      console.log(e);
+    },
+    onFilter: e => {
+      console.log(e);
+    },
+    onMove: e => {
+      console.log(e);
+    },
+    onClone: e => {
+      console.log(e);
+    },
+    onChange: e => {
+      console.log(e);
+    }
+  });
 }
